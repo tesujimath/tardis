ede13dbf11c8f8380f7ca4f0b3ecf98ec48ffe7c
Simon Guest <simon.guest@tesujimath.org>
2018-03-12 11:02:51 +1300

    Initial packaging for PyPI, excluding package restructuring.

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# setuptools setup script for tardis
+
+from setuptools import setup
+
+setup(name='tardis',
+      use_scm_version=True,
+      setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
+      description='Front-end for bioinformatics HPC job submission',
+      author='Alan McCulloch',
+      author_email='alan.mcculloch@agresearch.co.nz',
+      url='https://github.com/AgResearch/tardis',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+      ],
+      packages=['tardis'],
+      scripts=['tardis.py','tardish'],
+      package_data={
+          'doc': ['doc/tardis.pdf'],
+          'test': ['*'],
+      },
+      license='GPLv2',
+      install_requires=[
+          'biopython',
+          'pyzmq',
+      ],
+      python_requires='>=2.7, <3',
+     )
